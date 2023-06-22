e336f9a7d742f4cf0028b92de712c85b94fac8cd
Jelle <jelle@victoor.io>
2023-06-22 22:41:14 +0200

    added watchdog

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from program import setup
+from src.weather.program import setup
+from machine import Pin
+from time import sleep
 
 if __name__ == "__main__":
+    machine_led = Pin('LED', Pin.OUT)
+    machine_led.value(1)
+    sleep(1)
+    machine_led.value(0)
     setup()
--- src/weather/program.py
+++ src/weather/program.py
@@ -1,19 +1,38 @@
-from src.weather.bme680 import BME680_I2C
-import network
-import machine
-import utime
-from machine import Pin, I2C, Timer
+import json
 from time import sleep
 
+import network
+from machine import Pin, I2C, Timer
+from machine import WDT
 from umqtt.robust import MQTTClient
-import json
 
+from src.weather.bme680 import BME680_I2C
 
+TIMEOUT = 5000
+rocker_count = 0
+rocker_modifier = 0.3274793067734472
+mqtt_server = '192.168.1.5'
+client_id = 'weatherstation'
+topic_pub = b'klskmp/buiten/weather'
+status_pub = b'klskmp/buiten/station'
+rocker = Pin(28, Pin.IN, Pin.PULL_UP)
+bme = None
+client = None
+
+wdt = WDT(timeout=TIMEOUT + 2000)  # set a timeout of 2s more
+
+## calculate surface to benchmark to know the rained mm
+
+# 36 rocks = 300 ml
+# rock = 300/36
+# surface_in_mm =  pow(180/2,2) * math.pi
+# ml_in_mm2 = surface_in_mm/1000
+# liter_modifier = rock/ml_in_mm2
 
 machine_led = Pin('LED', Pin.OUT)
 
 
-def tipped(pin):
+def tipped():
     global rocker_count
     rocker_count = rocker_count + 1
     print("rocker triggered")
@@ -27,8 +46,8 @@ def mqtt_connect():
 
 
 def send(timer):
-    machine_led.value(1)
     global rocker_count
+    machine_led.value(1)
     no_of_rocks_to_send = rocker_count
     temp = round(bme.temperature, 2)
     hum = round(bme.humidity, 2)
@@ -44,10 +63,10 @@ def send(timer):
     try:
         client.publish(topic_pub, json.dumps(data_to_send))
         print(json.dumps(data_to_send))
+        wdt.feed()  # make sure the watchdog does not shut it down
         machine_led.value(0)
     except OSError as e:
         print("failed to connect")
-        reconnect()
 
     rocker_count = rocker_count - no_of_rocks_to_send
 
@@ -58,50 +77,26 @@ def start_up_sequence():
         sleep(0.1)
         machine_led.value(0)
         sleep(0.1)
-    
+
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect("victoor", "victoor123")
+    tries = 0
 
-    while not wlan.isconnected() and wlan.status() >= 0:
+    while not wlan.isconnected() and wlan.status() >= 0 and tries < 10:
         print("Waiting to connect:")
         for i in range(3):
             machine_led.value(1)
             sleep(0.01)
             machine_led.value(0)
             sleep(0.01)
-            
+        tries = tries + 1
+        wdt.feed()
+
     machine_led.value(0)
     print("connected to wlan")
     sleep(3)
-
-
-def reconnect():
-    print('Failed to connect to the MQTT Broker. Reconnecting...')
-    for i in range(3):
-        machine_led.value(1)
-        sleep(1)
-        machine_led.value(0)
-        sleep(1)
-    machine.reset()
-    # 36 rocks = 0.3 l
-    # 1 rock = 0.0083 l
-    # rock = 0.3/36
-    # surface_in_mm =  pow(180/2,2) * math.pi
-    # surface_exploder = 1000000/surface_in_mm
-    # liter_modifier = surface_exploder * rock
-
-
-
-
-rocker_count = 0
-rocker_modifier = 0.3274793067734472
-mqtt_server = '192.168.1.5'
-client_id = 'weatherstation'
-topic_pub = b'klskmp/buiten/weather'
-rocker = Pin(28, Pin.IN, Pin.PULL_UP)
-bme = None
-client = None
+    wdt.feed()
 
 
 def setup():
@@ -112,8 +107,9 @@ def setup():
     i2c = I2C(id=0, scl=Pin(17), sda=Pin(16))
     bme = BME680_I2C(i2c=i2c)
     client = mqtt_connect()
+    client.publish(status_pub, json.dumps({"status": "startup"}))
+    wdt.feed()  # make sure the watchdog does not shut it down
     rocker.irq(tipped)
     timer = Timer(-1)
-    timer.init(period=5000, mode=Timer.PERIODIC, callback=send)
-
+    timer.init(period=TIMEOUT, mode=Timer.PERIODIC, callback=send)
 
